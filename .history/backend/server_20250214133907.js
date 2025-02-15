049ae972f0a1701c5c3f44c768acbeda49ae6c9d
golfstrimmar <yushinbox@gmail.com>
2025-02-15 05:50:14 +0100

    ррррррррррр

--- .history/backend/server_20250214133907.js
+++ .history/backend/server_20250214133907.js
@@ -179,3 +179,70 @@ io.on("connection", (socket) => {
 server.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+// import express from "express";
+// import cors from "cors";
+// import { connectDB } from "./config/db.js";
+// import multer from "multer";
+// import { Server } from "socket.io";
+// import http from "http";
+
+// const upload = multer();
+// connectDB();
+// const app = express();
+// const server = http.createServer(app);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     // origin: "https://react-blog-beryl-nu.vercel.app",
+//     // origin: "*",
+//     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
+// // -------------------------------------
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   },
+//   path: "/socket.io",
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`Socket connected: ${socket.id}`);
+//   socket.on("disconnect", () => {
+//     console.log(`Socket disconnected: ${socket.id}`);
+//   });
+// });
+
+// // -------------------------------------
+// // расшифровка запроса
+// app.use((req, res, next) => {
+//   console.log(
+//     `Incoming request:  method=${req.method} url=${req.url}   body=${
+//       req.body
+//     }    body=${JSON.stringify(req.body, null, 2)}`
+//   );
+//   next();
+// });
+// // -------------------------------------
+// // ===================================
+// // io.on("connect", (socket) => {
+// //   console.log(`*******************************************************`);
+// //   console.log(`*+*on Connection id:*+* ${socket.id}`);
+// //   // socket.emit("receiveStunden");
+// //   // =================================================
+// //   socket.on("disconnect", () => {
+// //     console.log(`*-*on  disconnect: socket.id,- ${socket.id} interrapted:*-* `);
+// //     console.log(`*******************************************************`);
+// //   });
+// // });
+// // ===================================
+// app.listen(5000, () => {
+//   console.log("Server is running on port 5000");
+// });
--- .history/backend/server_20250215054357.js
+++ .history/backend/server_20250215054357.js
@@ -179,3 +179,4 @@ io.on("connection", (socket) => {
 server.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
